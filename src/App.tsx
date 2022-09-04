5c03f8e984bde99bc1732e0b60da65cacfdbf73c
David Rönnlid <david.ronnlid@gmail.com>
2022-09-04 11:48:36 +0200

    Auth functionality now works locally & in prod

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Reset from "./components/Reset";
-import Dashboard from "./components/Dashboard";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Reset from "./components/auth/Reset";
+import Dashboard from "./components/auth/Dashboard";
 function App() {
   return (
     <div className="app">
